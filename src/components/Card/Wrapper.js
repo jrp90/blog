724db10e8f2ffc215857422289ff6717dfe7e82e
James Pettifer <james.pettifer9@gmail.com>
2018-02-23 20:22:59 +0000

    Ski post

--- src/components/Card/Wrapper.js
+++ src/components/Card/Wrapper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   background: white;
   box-shadow: 5px 5px 10px rgba(0,0,0,0.1);
+  height: 100%;
 `;
 
 export default Wrapper;
