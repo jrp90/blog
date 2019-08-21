f3c0efc487d6fa88cbc8668b7ae8da8af93f4f43
James Pettifer <jpettifer@helical-levity.com>
2019-08-21 18:15:41 +0100

    add rel me to social links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,41 +11,53 @@ import github from './github.svg';
 import rss from './rss.svg';
 
 class Footer extends React.Component {
- render() {
-  return (
-   <Wrapper>
-    <Container>
-     <Content>
-      <Bio>
-       <img src={james} alt="James Pettifer" />
-       <p>Hi, I'm James, a frontend web developer based in Nottingham, UK</p>
-      </Bio>
-      <Social
-       first
-       href="https://www.instagram.com/jrp09u/"
-       title="Instagram"
-       target="_blank"
-      >
-       <img src={instagram} alt="Instagram" />
-       <p>@jrp09u</p>
-      </Social>
-      <Social href="https://twitter.com/jrp90" title="Twitter" target="_blank">
-       <img src={twitter} alt="Twitter" />
-       <p>@jrp90</p>
-      </Social>
-      <Social href="https://github.com/jrp90" title="Github" target="_blank">
-       <img src={github} alt="Github" />
-       <p>@jrp90</p>
-      </Social>
-      <Social href="/rss.xml" title="RSS Feed">
-       <img src={rss} alt="RSS" />
-       <p>RSS</p>
-      </Social>
-     </Content>
-    </Container>
-   </Wrapper>
-  );
- }
+  render() {
+    return (
+      <Wrapper>
+        <Container>
+          <Content>
+            <Bio>
+              <img src={james} alt="James Pettifer" />
+              <p>
+                Hi, I'm James, a frontend web developer based in Nottingham, UK
+              </p>
+            </Bio>
+            <Social
+              first
+              href="https://www.instagram.com/jrp09u/"
+              title="Instagram"
+              target="_blank"
+            >
+              <img src={instagram} alt="Instagram" />
+              <p>@jrp09u</p>
+            </Social>
+            <Social
+              href="https://twitter.com/jrp90"
+              title="Twitter"
+              target="_blank"
+              rel="me"
+            >
+              <img src={twitter} alt="Twitter" />
+              <p>@jrp90</p>
+            </Social>
+            <Social
+              href="https://github.com/jrp90"
+              title="Github"
+              target="_blank"
+              rel="me"
+            >
+              <img src={github} alt="Github" />
+              <p>@jrp90</p>
+            </Social>
+            <Social href="/rss.xml" title="RSS Feed">
+              <img src={rss} alt="RSS" />
+              <p>RSS</p>
+            </Social>
+          </Content>
+        </Container>
+      </Wrapper>
+    );
+  }
 }
 
 export default Footer;
