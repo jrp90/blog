08edb0dfa04bebf3785485562c72015c9a5d1bfb
James Pettifer <jpettifer@helical-levity.com>
2019-08-21 18:36:10 +0100

    try a couple of microformat tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -12,55 +12,55 @@ import Back from '../components/Back';
 import { rhythm, scale } from '../utils/typography';
 
 class BlogPostTemplate extends React.Component {
- render() {
-  const post = this.props.data.markdownRemark;
-  const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+  render() {
+    const post = this.props.data.markdownRemark;
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
 
-  return (
-   <Layout>
-    <PoseGroup animateOnMount={true}>
-     <Container key="container">
-      <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-      <BackLink to="/">
-       <Back />
-       <span>Back Home</span>
-      </BackLink>
-      <Header>{post.frontmatter.title}</Header>
-      <p
-       style={{
-        ...scale(-1 / 5),
-        display: 'block',
-        marginBottom: rhythm(1),
-        marginTop: rhythm(-1 / 2),
-       }}
-      >
-       {post.frontmatter.date}
-      </p>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-     </Container>
-    </PoseGroup>
-   </Layout>
-  );
- }
+    return (
+      <Layout>
+        <PoseGroup animateOnMount={true}>
+          <Container key="container" className="h-card">
+            <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
+            <BackLink to="/">
+              <Back />
+              <span>Back Home</span>
+            </BackLink>
+            <Header className="p-name">{post.frontmatter.title}</Header>
+            <p
+              style={{
+                ...scale(-1 / 5),
+                display: 'block',
+                marginBottom: rhythm(1),
+                marginTop: rhythm(-1 / 2),
+              }}
+            >
+              {post.frontmatter.date}
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          </Container>
+        </PoseGroup>
+      </Layout>
+    );
+  }
 }
 
 export default BlogPostTemplate;
 
 export const pageQuery = graphql`
- query BlogPostBySlug($slug: String!) {
-  site {
-   siteMetadata {
-    title
-    author
-   }
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
   }
-  markdownRemark(fields: { slug: { eq: $slug } }) {
-   id
-   html
-   frontmatter {
-    title
-    date(formatString: "MMMM DD, YYYY")
-   }
-  }
- }
 `;
